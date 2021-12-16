9b0856dcf5c1f12af0a51fca3f978ba6117b6da0
Conklaven <jacob.klaven@gmail.com>
2021-12-16 12:34:14 +0200

    Daily Challenge 16122021 1234

---
+++ Week2/Day5/DailyChallenge/myscripts.js
@@ -0,0 +1,18 @@
+let start;
+let i = 1;
+
+function beer() {
+    start = Number(prompt(" How many bottles of beer do you have?"));
+    console.log(`${start} bottles of beer on the wall`)
+    console.log(`${start} bottles of beer`)
+    console.log(`take ${i} down pass it around`)
+    
+    for (i = 1; i <= start; i++) {
+        console.log(`${start - i} bottles of beer on the wall`)
+        console.log(" ")
+        console.log(`${start - i} bottles of beer on the wall`)
+        console.log(`${start - i} bottles of beer`)
+        console.log(`take ${i} down pass it around`)
+    }
+}
+beer()
