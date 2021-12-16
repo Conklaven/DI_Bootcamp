124ed66aa8f65a631bb54289739debd0a71036d6
Conklaven <jacob.klaven@gmail.com>
2021-12-16 12:54:03 +0200

    Daily Challenge 16122021 1300

--- Week2/Day5/DailyChallenge/myscripts.js
+++ Week2/Day5/DailyChallenge/myscripts.js
@@ -1,18 +1,27 @@
 let start;
-let i = 1;
 
 function beer() {
     start = Number(prompt(" How many bottles of beer do you have?"));
-    console.log(`${start} bottles of beer on the wall`)
-    console.log(`${start} bottles of beer`)
-    console.log(`take ${i} down pass it around`)
-    
+
     for (i = 1; i <= start; i++) {
-        console.log(`${start - i} bottles of beer on the wall`)
-        console.log(" ")
-        console.log(`${start - i} bottles of beer on the wall`)
-        console.log(`${start - i} bottles of beer`)
-        console.log(`take ${i} down pass them around`)
+        if (i == 1) {
+            console.log(`${start} bottles of beer on the wall`)
+            console.log(`${start} bottles of beer`)
+            console.log(`take ${i} down pass it around`)
+            console.log(`${start - i} bottles of beer on the wall`)
+            console.log(" ")
+        } else if (i > 1 && i < start) {
+            console.log(`${start - (i -1)} bottles of beer on the wall`)
+            console.log(`${start - (i -1)} bottles of beer`)
+            console.log(`take ${i} down pass them around`)
+            console.log(`${start - i} bottles of beer on the wall`)
+            console.log(" ")
+        } else {
+            console.log(`${start - (i -1)} bottle of beer on the wall`)
+            console.log(`${start - (i -1)} bottle of beer`)
+            console.log(`take ${i} down pass them around`)
+            console.log(`No more bottles of beer on the wall`)
+        }
     }
 }
 beer()
