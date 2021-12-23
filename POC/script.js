19915e079ae3cd43ca01da5a2b133fc78b0d8cbe
Conklaven <jacob.klaven@gmail.com>
2021-12-23 19:33:54 +0200

    buy now

--- POC/script.js
+++ POC/script.js
@@ -1,61 +1,52 @@
-let topping = document.getElementById("imgtwo");
+let restore = document.body.innerHTML
+console.log(restore)
+
 let toppingTwo = document.getElementById("imgthree");
 let toppingThree = document.getElementById("imgfour");
+let toppingFour = document.getElementById("imgfive");
+let toppingFive = document.getElementById("imgsix");
+let toppingSix = document.getElementById("imgseven");
 
-topping.addEventListener("dragstart", startToDrag);
-// topping.addEventListener("dragend", endDrag);
+
 toppingTwo.addEventListener("dragstart", startToDrag);
-// toppingTwo.addEventListener("dragend", endDrag);
 toppingThree.addEventListener("dragstart", startToDrag);
-// toppingThree.addEventListener("dragend", endDrag);
+toppingFour.addEventListener("dragstart", startToDrag);
+toppingFive.addEventListener("dragstart", startToDrag);
+toppingSix.addEventListener("dragstart", startToDrag);
+
+
 
 function startToDrag(e) {
     e.dataTransfer.setData("text/plain", e.target.id);
-//    e.dataTransfer.effectAllowed = "move"; 
     console.log(e.target.id);
 }
 
 let dropbox = document.getElementById("imgOne");
-console.log(dropbox)
-
-// dropbox.addEventListener("dragenter", enter)
-// target.addEventListener("dragleave", exitBox)
 dropbox.addEventListener("dragover", over)
 dropbox.addEventListener("drop", dropping)
 
 function over(e) {
     e.preventDefault();
-    // e.dataTransfer.dropEffect= "move";
     console.log("hi")
 }
 
-// function enter(e) {
-//     e.preventDefault();
-
-// }
-
+let price = document.getElementById("actualPrice")
 
 function dropping(e) {
     // e.preventDefault();
     let dataItem = e.dataTransfer.getData("text/plain");
-    console.log("dataItem", dataItem)
     let draggedItem = document.getElementById(dataItem);
-    console.log("e.target : ", e.target)
     dropbox.appendChild(draggedItem);
     let moved = document.getElementById(dataItem)
     moved.classList.add("after");
-    
+    price.textContent = (parseFloat(price.textContent) + 5).toFixed(2);
+    let p = moved.id
+    let label = document.getElementById(`label${p}`)
+    label.style.display = "none"
 }
-
-// function endDrag(e) {
-//     e.target.classList.add("after")
-
-// }
-
 let reset = document.getElementById("reset")
 reset.addEventListener("click", resetPizza)
 
-function resetPizza(e) {
-    let items = document.querySelectorAll(".after > *")
-    items.classList.remove("after")
+function resetPizza() {
+    window.location.reload();
 }
