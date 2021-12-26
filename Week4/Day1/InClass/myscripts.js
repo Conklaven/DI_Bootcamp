cf0fca302853af58d4845906a99ace5d969e45d1
Conklaven <jacob.klaven@gmail.com>
2021-12-26 13:05:01 +0200

    daily work

---
+++ Week4/Day1/InClass/myscripts.js
@@ -0,0 +1,69 @@
+// //1st exercise
+// //You need to guess what will be console logged, and explain why
+
+
+// let username = "John";
+
+// function checkName (){
+// 	console.log("In the function",username); //john
+// 	username += "Smith";
+// 	console.log("Still in the function",username); //johmnsmith
+// }
+
+// console.log("before the function", username); // john
+
+// checkName()
+
+// console.log("after the function", username); //johnsmith
+
+
+
+// //2ns exercise
+// //You need to guess what will be console logged, and explain why
+
+
+// let username = "John";
+
+// function checkName (){
+// 	let username = "Sarah";
+// 	console.log("In the function",username); //sarah
+// 	username += "Smith";
+// 	console.log("Still in the function",username); //sarahsmith
+// }
+
+// console.log("before the function", username); //john
+
+// checkName()
+
+// console.log("after the function", username); //john
+
+// const employees = [
+// 	{name: "Anne", email: "Anna@example.com", phone:"0000–000–0000"},
+// 	{name: "Tom", email: "Tom@gmail.com", phone:"0111–111–1111"},
+// ]
+
+// for (data of employees) {
+//     let {name, email, phone} = data
+//     console.log(name)
+// console.log(email)
+// console.log(phone)
+// }
+
+// 1. Greet the user (ie. using an argument),
+// * first by using function declarations
+// * then function expression,
+// * then arrow function
+let btn= document.getElementById("btn")
+// btn.addEventListener("click", usergreet)
+// function usergreet(){
+//     alert("Hello User")
+// }
+
+btn.addEventListener("click", function(){
+    alert("hello User")
+})
+
+// btn.addEventListener("click", () => alert("hello User"))
+
+
+
