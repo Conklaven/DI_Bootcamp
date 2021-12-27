f36a7efae5982fb8165d67995184f0ff6cce5880
Conklaven <jacob.klaven@gmail.com>
2021-12-27 21:32:24 +0200

    XP

---
+++ Week4/Day2/Exercises/XP/myscripts.js
@@ -0,0 +1,13 @@
+// --------------------------- Exercise 1 -----------------------------
+let add =(a,b) => a + b;
+let result = add(7,3)
+console.log(result)
+
+// --------------------------- Exercise 2 -----------------------------
+const addTo = x => y => x + y;
+var addToTen = addTo(10);
+addToTen(3);
+console.log(addTo)
+console.log(addToTen)
+
+// --------------------------- Exercise 3 -----------------------------
