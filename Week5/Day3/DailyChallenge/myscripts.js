64229930b588bf7a72886c883a126b1e6b5b0403
Conklaven <jacob.klaven@gmail.com>
2022-01-05 08:39:51 +0200

    daily challenge

--- Week5/Day3/DailyChallenge/myscripts.js
+++ Week5/Day3/DailyChallenge/myscripts.js
@@ -15,22 +15,115 @@ function makeAllCaps(array) {
     return newprom
 }
 
-    makeAllCaps(arrayOfWords)
-      .then(sortWords)
-      .then((result) => console.log(result))
-      .catch(error => console.log(error))
+makeAllCaps(arrayOfWords)
+    .then(sortWords)
+    .then((result) => console.log(result))
+    .catch(error => console.log(error))
 
 
-      
-      function sortWords(e) {
-        let newprom = new Promise(function (resolve, reject) {
-            if (e.length == e.length) {
-                resolve(e.sort());
-            } else {
-                reject(e);
+
+function sortWords(e) {
+    let newprom = new Promise(function (resolve, reject) {
+        if (e.length == e.length) {
+            resolve(e.sort());
+        } else {
+            reject(e);
+        }
+
+    })
+
+    return newprom
+}
+
+// part 2
+
+let morse = `{
+        "0": "-----",
+        "1": ".----",
+        "2": "..---",
+        "3": "...--",
+        "4": "....-",
+        "5": ".....",
+        "6": "-....",
+        "7": "--...",
+        "8": "---..",
+        "9": "----.",
+        "a": ".-",
+        "b": "-...",
+        "c": "-.-.",
+        "d": "-..",
+        "e": ".",
+        "f": "..-.",
+        "g": "--.",
+        "h": "....",
+        "i": "..",
+        "j": ".---",
+        "k": "-.-",
+        "l": ".-..",
+        "m": "--",
+        "n": "-.",
+        "o": "---",
+        "p": ".--.",
+        "q": "--.-",
+        "r": ".-.",
+        "s": "...",
+        "t": "-",
+        "u": "..-",
+        "v": "...-",
+        "w": ".--",
+        "x": "-..-",
+        "y": "-.--",
+        "z": "--..",
+        ".": ".-.-.-",
+        ",": "--..--",
+        "?": "..--..",
+        "!": "-.-.--",
+        "-": "-....-",
+        "/": "-..-.",
+        "@": ".--.-.",
+        "(": "-.--.",
+        ")": "-.--.-"
+      }`
+
+let object;
+function toJs() {
+    let morseJS = new Promise(function (resolve, reject) {
+        if (morse != "") {
+            object = JSON.parse(morse)
+            console.log(object)
+            resolve(object)
+        } else {
+            reject(morse)
+        }
+
+    })
+
+    return morseJS
+}
+toJs()
+
+function toMorse(morseJS) {
+    console.log(morseJS)
+    let newMorse = new Promise(function (resolve, reject) {
+        let ask = prompt("enter a word")
+        if (morseJS != undefined) {
+            let morseCode = []
+            for (let i = 0; i < ask.length; i++) {
+                morseCode += ask[i]
+                let lower =ask[i].toLowerCase()
+                console.log(object)
+                console.log(object.find(lower))
             }
-    
-        })
-    
-        return newprom
-    }
+            console.log(morseCode)
+            resolve(morseCode)
+        } else {
+            reject(morse)
+        }
+
+    })
+
+    return morseJS
+}
+
+toMorse(object)
+// joinWords()
