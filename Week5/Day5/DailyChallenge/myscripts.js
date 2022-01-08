ea2ee8d0ce69b1785c59ed3e87f8009b268355bf
Conklaven <jacob.klaven@gmail.com>
2022-01-08 20:52:09 +0200

    XP and Daily

--- Week5/Day5/DailyChallenge/myscripts.js
+++ Week5/Day5/DailyChallenge/myscripts.js
@@ -1,4 +1,4 @@
-async function runFetch() {
+async function runFetch(a,b) {
     let fetched = await fetch(`http://api.currencylayer.com/live?access_key=f5a2d13309d2fce56ca45fcccf6450f2&format=1`)
     let response = fetched.json();
     console.log(response);
@@ -7,7 +7,7 @@ async function runFetch() {
 
 let convert = document.getElementById("convert");
 convert.addEventListener("click", getData)
-
+let array ={}
 function getData(e) {
     e.preventDefault();
     let from = document.getElementById("fromcurrency").value
@@ -17,12 +17,47 @@ function getData(e) {
     let amount = (document.getElementById("amount").value)
     let amountInt = parseInt(amount);
     console.log(typeof amountInt)
-    runFetch()
-    .then(result => result.quotes)
-    .then(quotes => {
-            let combined = from,to;
+    runFetch(from, to)
+    .then(result => {
+        let key = from + to;
+        let value = result.quotes[key]
+        let resultspace = document.getElementById("result")
+        resultspace.style.display ="block"
+        resultspace.innerHTML = (amountInt * value).toFixed(2)
     })
+    // .then(quotes => {
+            
+    //         array = quotes
+    //         getExchange(array, key)
+    // })
 }
 
-// runFetch()
+// runFetch(a,b)
 // .then((result) => { console.log(result) })
+// function getExchange(a, key) {
+//     let value = a.key 
+//     console.log(a.key)
+//     console.log(a)
+//     console.log(key)
+// }
+
+async function getCurrency() {
+    let fetched = await fetch(`http://api.currencylayer.com/list?access_key=f5a2d13309d2fce56ca45fcccf6450f2`)
+    let response = fetched.json();
+    console.log(response);
+    return response
+}
+
+getCurrency()
+.then(response => response.currencies)
+.then(array => {
+    console.log(array);
+    
+    for (let key in array)  {
+        let option = document.createElement('option')
+        option.setAttribute('value', key)
+        option.innerHTML = array[key]
+        let parent = document.getElementById("tocurrency")
+        parent.appendChild(option)
+    }
+})
